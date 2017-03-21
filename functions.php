8e767d54a92fa1d4c02718922762562d67cd980b
Ahmed Mohammed <amElnagdy@users.noreply.github.com>
2017-03-21 15:08:35 +0200

    update divicolt_enqueue_styles()
    force child theme's style.css to be loaded

--- functions.php
+++ functions.php
@@ -3,8 +3,13 @@
 	DiviColt Functions
 */
 function divicolt_enqueue_styles() {
-	$parent_style = 'divi-style';
-	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+wp_enqueue_style( 'child-style',
+get_stylesheet_directory_uri() . '/style.css',
+array( 'parent-style' ),
+wp_get_theme()->get('Version')
+);
 }
 add_action( 'wp_enqueue_scripts', 'divicolt_enqueue_styles' );
 
